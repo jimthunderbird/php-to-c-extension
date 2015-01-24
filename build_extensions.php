323d88d2101ec86107dbfc32b8c93b6e1b0664f3
jim huang <jim.javathunderbird@gmail.com>
2015-01-23 18:50:32 -0800

    documentation update

--- build_extensions.php
+++ build_extensions.php
@@ -41,7 +41,7 @@ foreach($extension_names as $extension_name) {
   if (chdir($zephir_project_dir) ) {
     echo shell_exec(__DIR__."/vendor/bin/php-to-zephir phpToZephir:convertDir .");
     echo shell_exec(__DIR__."/vendor/bin/zephir build");
-    //cleap up temporary directories
+    //cleap up temporary php files
     shell_exec("rm -f $(find  $zephir_project_dir -type f -name \"*.php\")");
   }
 }
