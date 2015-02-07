04e0a3627c52552d7f67f5f1e8a1596afeca6d64
jim huang <jim.javathunderbird@gmail.com>
2015-02-07 14:29:26 -0800

    add code reformat converter

---
+++ src/PHPtoCExt/Converter/CodeReformatConverter.php
@@ -0,0 +1,44 @@
+<?php 
+/**
+ * Reformat the code so that class properties come first and class methods come later 
+ */
+namespace PHPtoCExt\Converter;
+
+class CodeReformatConverter extends \PHPtoCExt\Converter
+{
+  public function convert()
+  {
+    $classMap = $this->getClassMap();
+
+    foreach($classMap as $className => $classInfo) {
+
+      $originalClassContent = implode("\n",array_slice($this->codeLines, $classInfo->startLine - 1, $classInfo->endLine - $classInfo->startLine + 1));
+
+      //maybe put them into a template ... ?
+      $indentation = str_repeat(" ",2);
+      $content = "namespace ".$classInfo->namespace.";\n";
+      $content .= "class ".array_pop(explode("\\",$classInfo->className))."\n";
+      $content .= "{\n";
+      
+      ///////// Static Properties //////////// 
+      foreach($classInfo->staticProperties as $propertyInfo) {
+        $content .= $propertyInfo->code."\n";
+      }
+      
+      //////// Instance Properties ////////// 
+      foreach($classInfo->properties as $propertyInfo) {
+        $content .= $propertyInfo->code."\n";
+      }
+
+      //////// Methods //////////
+      foreach($classInfo->methodInfos as $methodInfo) {
+        $content .= implode("\n",array_slice($this->codeLines, $methodInfo->startLine - 1, $methodInfo->endLine - $methodInfo->startLine + 1));
+        $content .= "\n"; 
+      }
+      
+      $content .= "}\n";
+
+      $this->searchAndReplace($originalClassContent, $content);
+    }
+  }
+}
