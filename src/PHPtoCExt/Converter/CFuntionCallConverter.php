cab9319cbf7238faf898ffb86abd72d8e28466f8
jim huang <jim.javathunderbird@gmail.com>
2015-02-17 22:12:56 -0800

    c function call component analysing

--- src/PHPtoCExt/Converter/CFuntionCallConverter.php
+++ src/PHPtoCExt/Converter/CFuntionCallConverter.php
@@ -19,12 +19,18 @@ class CFuntionCallConverter extends \PHPtoCExt\Converter
       for ($index = $classInfo->startLine; $index <= $classInfo->endLine; $index++) {
         if (preg_match("/call_c_function\(.*\)/", $this->codeLines[$index], $matches)) {
           if(count($matches) == 1) {
-            $lineComps = explode("=",$this->codeLines[$index]);
+            $codeLine = str_replace(array("call_c_function","(",")",";"),"",trim($this->codeLines[$index]));
+            $lineComps = explode("=",$codeLine);
             $lineCompsCount = count($lineComps);
             if ($lineCompsCount == 1) { //this means we just call the c function and do not have a return variable
-              
-            } else if ($lineCompsCount == 2) { //this means we call the c function and then store the result in a return variable
-
+              $cFunctionCallComps = explode(",",$lineComps[0]);
+            } else if ($lineCompsCount > 1) { //this means we call the c function and then store the result in a return variable 
+              $resultVarName = trim(str_replace("$","",$lineComps[0]));
+              $cFunctionCallComps = explode(",",$lineComps[1]);
+              foreach($cFunctionCallComps as $idx => $comp) {
+                $cFunctionCallComps[$idx] = trim(str_replace(array("'",'"',"$"),"",$cFunctionCallComps[$idx]));
+              }
+              print_r($cFunctionCallComps);exit();
             }
           }
         }
