63f1377cc0fd5f6be495e03c65e271a499dd55a2
jim huang <jim.javathunderbird@gmail.com>
2015-02-18 11:31:57 -0800

    generate expected function call

--- src/PHPtoCExt/Converter/CFuntionCallConverter.php
+++ src/PHPtoCExt/Converter/CFuntionCallConverter.php
@@ -22,20 +22,43 @@ class CFuntionCallConverter extends \PHPtoCExt\Converter
             $codeLine = str_replace(array("call_c_function","(",")",";"),"",trim($this->codeLines[$index]));
             $lineComps = explode("=",$codeLine);
             $lineCompsCount = count($lineComps);
+            $resultVarName = "";
             if ($lineCompsCount == 1) { //this means we just call the c function and do not have a return variable
               $cFunctionCallComps = explode(",",$lineComps[0]);
+              $cFunctionCallComps = explode(",",$lineComps[1]);
+              foreach($cFunctionCallComps as $idx => $comp) {
+                $cFunctionCallComps[$idx] = trim(str_replace(array("'",'"',"$"),"",$cFunctionCallComps[$idx]));
+              }
             } else if ($lineCompsCount > 1) { //this means we call the c function and then store the result in a return variable 
               $resultVarName = trim(str_replace("$","",$lineComps[0]));
               $cFunctionCallComps = explode(",",$lineComps[1]);
               foreach($cFunctionCallComps as $idx => $comp) {
                 $cFunctionCallComps[$idx] = trim(str_replace(array("'",'"',"$"),"",$cFunctionCallComps[$idx]));
               }
-              print_r($cFunctionCallComps);exit();
             }
+
+            if (count($cFunctionCallComps) > 0) {
+              $cSourceFile = array_shift($cFunctionCallComps);
+              $cFUnctionName = array_shift($cFunctionCallComps);
+              $cFUnctionInputParamsStr = "";
+              if (count($cFunctionCallComps) > 0) {
+                $cFUnctionInputParamsStr = implode(",",$cFunctionCallComps); 
+              }
+              $cFunctionCallCode = "\n%{\n";
+              if (strlen($resultVarName) == 0) {
+                $cFunctionCallCode .= $cFUnctionName."($cFUnctionInputParamsStr);";                 
+              } else {
+                $cFunctionCallCode .= "$resultVarName = ".$cFUnctionName."($cFUnctionInputParamsStr);";
+              }
+              $cFunctionCallCode .=  "\n}%\n";
+              print $cFunctionCallCode;
+            }
+
           }
+
         }
       }
-    }
 
+    }
   }
 }
