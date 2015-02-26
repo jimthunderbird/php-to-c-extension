24fb947611095cea58461470933ddb56687336d4
jim huang <jim.javathunderbird@gmail.com>
2015-02-26 00:12:50 -0800

    set up tmp c function result var only once

--- src/PHPtoCExt/Converter/CFuntionCallConverter.php
+++ src/PHPtoCExt/Converter/CFuntionCallConverter.php
@@ -18,83 +18,94 @@ class CFuntionCallConverter extends \PHPtoCExt\Converter
     $cSourceCodeMap = array();
 
     foreach($classMap as $className => $classInfo) {
-      for ($index = $classInfo->startLine; $index <= $classInfo->endLine; $index++) {
-        if (preg_match("/call_c_function\(.*\)/", $this->codeLines[$index], $matches)) {
-          if(count($matches) == 1) {
-            $codeLine = str_replace(array("call_c_function","(",")",";","$"),"",trim($this->codeLines[$index]));
-            $lineComps = explode("=",$codeLine);
-            $lineCompsCount = count($lineComps);
-            $resultVarName = "";
-            if ($lineCompsCount == 1) { //this means we just call the c function and do not have a return variable
-              $cFunctionCallComps = explode(",",$lineComps[0]);
-              foreach($cFunctionCallComps as $idx => $comp) {
-                $cFunctionCallComps[$idx] = trim(str_replace("$","",$cFunctionCallComps[$idx]));
-              }
-            } else if ($lineCompsCount > 1) { //this means we call the c function and then store the result in a return variable 
-              $resultVarName = trim(str_replace("$","",$lineComps[0]));
-              $cFunctionCallComps = explode(",",$lineComps[1]);
-              foreach($cFunctionCallComps as $idx => $comp) {
-                $cFunctionCallComps[$idx] = trim(str_replace("$","",$cFunctionCallComps[$idx]));
-              }
-            }
+      $originalClassCode = implode("\n",array_slice($this->codeLines, $classInfo->startLine - 1, $classInfo->endLine - $classInfo->startLine + 1)); 
+      foreach($classInfo->methodInfos as $methodPureName => $methodInfo) {
+        $tmpCFuncCallResultDefined = false;
+        for ($index = $methodInfo->startLine; $index <= $methodInfo->endLine; $index++) {
+          if (preg_match("/call_c_function\(.*\)/", $this->codeLines[$index], $matches)) {
 
-            if (count($cFunctionCallComps) > 0) {
-              $firstComp = array_shift($cFunctionCallComps);
-              $cSourceFile = str_replace(array("'",'"'),"",$firstComp);
-              $secondComp = array_shift($cFunctionCallComps);
-              $cFUnctionName = str_replace(array("'",'"'),"",$secondComp);
-              $cFUnctionInputParamsStr = "";
+            //we simply add $tmpCFuncCallResult = null;\n at the beginning of the method 
+            if (!$tmpCFuncCallResultDefined) {
+              $this->codeLine[$index] = "\$tmpCFuncCallResult = null\n".$this->codeLines[$index];
+              $tmpCFuncCallResultDefined = true;
+            } 
+
+            if(count($matches) == 1) {
+              $codeLine = str_replace(array("call_c_function","(",")",";","$"),"",trim($this->codeLines[$index]));
+              $lineComps = explode("=",$codeLine);
+              $lineCompsCount = count($lineComps);
+              $resultVarName = "";
+              if ($lineCompsCount == 1) { //this means we just call the c function and do not have a return variable
+                $cFunctionCallComps = explode(",",$lineComps[0]);
+                foreach($cFunctionCallComps as $idx => $comp) {
+                  $cFunctionCallComps[$idx] = trim(str_replace("$","",$cFunctionCallComps[$idx]));
+                }
+              } else if ($lineCompsCount > 1) { //this means we call the c function and then store the result in a return variable 
+                $resultVarName = trim(str_replace("$","",$lineComps[0]));
+                $cFunctionCallComps = explode(",",$lineComps[1]);
+                foreach($cFunctionCallComps as $idx => $comp) {
+                  $cFunctionCallComps[$idx] = trim(str_replace("$","",$cFunctionCallComps[$idx]));
+                }
+              }
+
               if (count($cFunctionCallComps) > 0) {
-                $cFUnctionInputParamsStr = implode(", ",$cFunctionCallComps); 
-              }
-
-              $cFunctionCallCode = "";
-              if (strlen($resultVarName) == 0) {
-                $cFunctionCallCode .= "\n%{\n";
-                $cFunctionCallCode .= $cFUnctionName."($cFUnctionInputParamsStr);";                 
-                $cFunctionCallCode .=  "\n}%\n";
-                if (strlen($cFUnctionInputParamsStr) == 0) { 
-                  $expectedZephirCode = 'call_c_function('.$firstComp.', '.$secondComp.');';
-                } else {
-                  $expectedZephirCode = 'call_c_function('.$firstComp.', '.$secondComp.', '.implode(", ",$cFunctionCallComps).');';
+                $firstComp = array_shift($cFunctionCallComps);
+                $cSourceFile = str_replace(array("'",'"'),"",$firstComp);
+                $secondComp = array_shift($cFunctionCallComps);
+                $cFUnctionName = str_replace(array("'",'"'),"",$secondComp);
+                $cFUnctionInputParamsStr = "";
+                if (count($cFunctionCallComps) > 0) {
+                  $cFUnctionInputParamsStr = implode(", ",$cFunctionCallComps); 
                 }
-              } else {
-                //use a tmp var to hold the c_function_call result 
-                $cFunctionCallCode .= "var tmpCFuncCallResult;\n";
-                $cFunctionCallCode .= "let tmpCFuncCallResult = null;\n";
-                $cFunctionCallCode .= "let $resultVarName = null;\n"; //initialize result var
-                $cFunctionCallCode .= "\n%{\n";
-                $cFunctionCallCode .= "tmpCFuncCallResult = ".$cFUnctionName."($cFUnctionInputParamsStr);\n";
-                $cFunctionCallCode .=  "\n}%\n";
-                $cFunctionCallCode .= "let $resultVarName = tmpCFuncCallResult;\n";
-                if (strlen($cFUnctionInputParamsStr) == 0) {
-                  $expectedZephirCode = 'let '.$resultVarName.' =  call_c_function('.$firstComp.', '.$secondComp.');';
+
+                $cFunctionCallCode = "";
+                if (strlen($resultVarName) == 0) {
+                  $cFunctionCallCode .= "\n%{\n";
+                  $cFunctionCallCode .= $cFUnctionName."($cFUnctionInputParamsStr);";                 
+                  $cFunctionCallCode .=  "\n}%\n";
+                  if (strlen($cFUnctionInputParamsStr) == 0) { 
+                    $expectedZephirCode = 'call_c_function('.$firstComp.', '.$secondComp.');';
+                  } else {
+                    $expectedZephirCode = 'call_c_function('.$firstComp.', '.$secondComp.', '.implode(", ",$cFunctionCallComps).');';
+                  }
                 } else {
-                  $expectedZephirCode = 'let '.$resultVarName.' =  call_c_function('.$firstComp.', '.$secondComp.', '.implode(", ",$cFunctionCallComps).');';
+                  $cFunctionCallCode .= "let $resultVarName = null;\n"; //initialize result var
+                  $cFunctionCallCode .= "\n%{\n";
+                  $cFunctionCallCode .= "tmpCFuncCallResult = ".$cFUnctionName."($cFUnctionInputParamsStr);\n";
+                  $cFunctionCallCode .=  "\n}%\n";
+                  $cFunctionCallCode .= "let $resultVarName = tmpCFuncCallResult;\n";
+                  if (strlen($cFUnctionInputParamsStr) == 0) {
+                    $expectedZephirCode = 'let '.$resultVarName.' =  call_c_function('.$firstComp.', '.$secondComp.');';
+                  } else {
+                    $expectedZephirCode = 'let '.$resultVarName.' =  call_c_function('.$firstComp.', '.$secondComp.', '.implode(", ",$cFunctionCallComps).');';
+                  }
+                }
+
+                $this->postSearchAndReplace($expectedZephirCode,$cFunctionCallCode);
+
+                //now, inject the c source code to the top of the class 
+                $namespace = $classInfo->namespace;
+                $classPureName = array_pop(explode("\\",$className));
+                $originalCode = "namespace $namespace;\n\n"."class $classPureName\n";
+                //make sure we only have one unique copy of c source file per class
+                $classNameCSourceFileKey = $className.".".$cSourceFile;
+                if (!isset($cSourceCodeMap[$classNameCSourceFileKey])) {
+                  $cSourceCode = file_get_contents($this->inputDir."/".$cSourceFile);
+                  $cSourceCodeMap[$classNameCSourceFileKey] = $cSourceCode;
+                  $withCSourceCode = "%{\n".$cSourceCodeMap[$classNameCSourceFileKey]."\n}%\n".$originalCode; 
+                  $this->postSearchAndReplace($originalCode, $withCSourceCode);
                 }
               }
 
-              $this->postSearchAndReplace($expectedZephirCode,$cFunctionCallCode);
-
-              //now, inject the c source code to the top of the class 
-              $namespace = $classInfo->namespace;
-              $classPureName = array_pop(explode("\\",$className));
-              $originalCode = "namespace $namespace;\n\n"."class $classPureName\n";
-              //make sure we only have one unique copy of c source file per class
-              $classNameCSourceFileKey = $className.".".$cSourceFile;
-              if (!isset($cSourceCodeMap[$classNameCSourceFileKey])) {
-                $cSourceCode = file_get_contents($this->inputDir."/".$cSourceFile);
-                $cSourceCodeMap[$classNameCSourceFileKey] = $cSourceCode;
-                $withCSourceCode = "%{\n".$cSourceCodeMap[$classNameCSourceFileKey]."\n}%\n".$originalCode; 
-                $this->postSearchAndReplace($originalCode, $withCSourceCode);
-              }
             }
 
           }
-
         }
-      }
 
+      }
+      
+      $currentClassCode = implode("\n",array_slice($this->codeLines, $classInfo->startLine - 1, $classInfo->endLine - $classInfo->startLine + 1)); 
+      $this->searchAndReplace($originalClassCode, $currentClassCode);
     }
   }
 }
